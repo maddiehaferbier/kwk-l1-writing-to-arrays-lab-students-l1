012096f0484b5378e83019a4faf8198b63c6f91e
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-06 21:02:24 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -1,10 +1,17 @@
-cart_item_prices = [12.43, 19.89, 3.49, 75.00]
-
-tax_included = []
-  
-cart_item_prices.each do |prices|
-  price_with_tax = prices * 1.08 
+#cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+#
+#tax_included = []
+#  
+#cart_item_prices.each do |prices|
+#  price_with_tax = prices * 1.08 
 #  tax_included << price_with_tax
 #end 
 #
 #puts tax_included
+
+cart_item_prices = [19.99, 75.0]
+cart_item_prices.each do |price|
+  big_ticket_prices << price
+end
+end
+  
