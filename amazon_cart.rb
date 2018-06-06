3343b35b3f657963a36d48ab1e0e12afedb4014c
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-06 20:34:06 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -2,3 +2,9 @@ cart_item_prices = [12.43, 19.89, 3.39, 75.00]
 cart_item_prices.push(5.3)
 
 puts cart_item_prices
+
+counts = 1
+cart_item_prices.each do |price| 
+  puts "item #{count}: #{prices}"
+  count += 1 
+end 
