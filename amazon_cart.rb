e390834b7d30cc937a16d4632e62e0ba298b5d04
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-06 20:53:41 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -3,7 +3,7 @@ cart_item_prices = [12.43, 19.89, 3.49, 75.00]
 tax_included = []
   
 cart_item_prices.each do |prices|
-  price_with_tax = price * 1.08 
+  price_with_tax = prices * 1.08 
   tax_included << price_with_tax
 end 
 
